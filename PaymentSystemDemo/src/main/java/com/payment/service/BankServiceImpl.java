8680054759a900aec358c1c720c13b2930b13071
Mamta-Reddy <61815173+Mamta-Reddy@users.noreply.github.com>
2022-09-18 15:05:00 +0000

    Add files via upload
    18/09/2022 ; 8:33 pm-Added service layer

---
+++ PaymentSystemDemo/src/main/java/com/payment/service/BankServiceImpl.java
@@ -0,0 +1,44 @@
+package com.payment.service;
+
+import java.util.List;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.payment.dao.BankRepository;
+import com.payment.entity.Bank;
+
+@Service
+public class BankServiceImpl implements BankService{
+	
+	@Autowired
+	BankRepository bankrepository;
+
+	@Override
+	public Bank addBankDetails(Bank bank) {
+		return bankrepository.save(bank);
+	}
+
+	@Override
+	public Bank updateBankDetails(Bank bank) {
+		return null;
+	}
+
+	@Override
+	public void deleteBank(String bic) {
+		bankrepository.deleteById(bic);
+	}
+
+	@Override
+	public List<Bank> getAllBankDetails() {
+		return bankrepository.findAll();
+	}
+
+	@Override
+	public Optional<Bank> getBankDetailsById(String bic) {
+		return bankrepository.findById(bic);
+	}
+
+}
